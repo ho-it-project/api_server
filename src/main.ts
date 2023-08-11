0b08272446fc7da6b7b0035c4f42ac3db39ba553
Denovo <vo0v0000@naver.com>
2023-08-11 21:07:20 +0900

    [TEST] swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const docs = require('../packages/api/swagger.json');
+  docs.servers = [{ url: 'http://localhost:3000/docs' }];
+
+  SwaggerModule.setup('docs', app, docs);
   await app.listen(3000);
 }
 bootstrap();
