15f9e106056bb24fae434c7975fbbaa04499a26f
Denovo <vo0v0000@naver.com>
2023-09-18 02:10:03 +0900

    [FIX] jenkins file and dockerfile e2e test

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { winstonLogger } from '@common/logger/logger';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = process.env.PORT || 8000;
   const app = await NestFactory.create(AppModule, { logger: winstonLogger });
-  //   const docs = require('../packages/api/swagger.json');
-  //   docs.servers = [{ url: 'http://localhost:8000/docs' }];
-  //   SwaggerModule.setup('docs', app, docs);
+  const docs = require('../packages/api/swagger.json');
+  docs.servers = [{ url: 'http://localhost:8000/docs' }];
+  SwaggerModule.setup('docs', app, docs);
   console.log(process.env.KAFKA_BOOTSTRAP_SERVERS?.split(',').map((a) => a.trim()));
   app.setGlobalPrefix('api');
   await app.listen(port);
