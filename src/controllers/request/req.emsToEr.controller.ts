30b65d72a2a1f8ad3178c395eccacafc2c3cc49a
Denovo <vo0v0000@naver.com>
2023-10-08 00:55:19 +0900

    docs: respondEmsToErRequest, getEmsToErRequestList docs 작성

--- src/controllers/request/req.emsToEr.controller.ts
+++ src/controllers/request/req.emsToEr.controller.ts
@@ -9,7 +9,7 @@ import { EmsJwtAccessAuthGuard } from '@src/auth/guard/ems.jwt.access.guard';
 import { ErJwtAccessAuthGuard } from '@src/auth/guard/er.jwt.access.guard';
 import { EmsAuth, ErAuth } from '@src/auth/interface';
 import { ReqEmsToErService } from '@src/providers/req/req.emsToEr.service';
-import { ReqEmsToErRequest, TryCatch } from '@src/types';
+import { ReqEmsToErRequest, Try, TryCatch } from '@src/types';
 import { ReqEmsToErResponse } from '@src/types/req.response.dto';
 
 @Controller('request/ems-to-er')
@@ -30,7 +30,7 @@ export class ReqEmsToErController {
    *
    * @author de-novo
    * @tag req_ems-to-er-(EMS)
-   * @summary EMS to Er 수용요청 생성 API -- 아직 완성안됨
+   * @summary 2023-10-08 - EMS to Er 수용요청 생성 API
    *
    * @security access_token
    * @param body
@@ -60,12 +60,47 @@ export class ReqEmsToErController {
     return createResponse(result);
   }
 
+  /**
+   * ems to er 요청 리스트 조회 API - EMS
+   *
+   * EMS 직원 본인이 생성한 요청 리스트를 조회하는 API 입니다.
+   *
+   * 본 API는 EMS 시스템에서만 호출 가능합니다.
+   * EMS 시스템은 본 API를 호출하여 EMS 직원 본인이 생성한 요청 리스트를 조회합니다.
+   * query를 통해 요청 리스트를 필터링 할 수 있습니다.
+   *
+   * ## query
+   *    - page?: number;
+   *        - default: 1
+   *    - limit?: number;
+   *        - default: 10
+   *    - search?: string;
+   *        - 검색어
+   *    - search_type?: 'ambulance_company_name' | 'patient_name' | 'patient_symptom_summary';
+   *        - 검색 타입
+   *    - request_status?: RequestStatus[];
+   *        - 요청 상태 필터링
+   *    - patient_gender?: Gender[];
+   *        - 환자 성별 필터링
+   *    - patient_severity?: ems_Severity[];
+   *        - 환자 중증도 필터링
+   *    - request_start_date?: string & tags.Format<'date-time'>; // 요청 시작 날짜 및 시간
+   *        - 요청 시작 날짜 및 시간
+   *
+   * @author de-novo
+   * @tag req_ems-to-er-(EMS)
+   * @summary 2023-10-08 EMS to Er 요청 리스트 조회 API - EMS
+   *
+   * @security access_token
+   * @returns ems to er 요청 리스트 및 요청 리스트 개수
+   */
   @TypedRoute.Get('/ems')
+  @TypedException<AUTH_ERROR.FORBIDDEN>(403, 'AUTH_ERROR.FORBIDDEN')
   @UseGuards(EmsJwtAccessAuthGuard)
   async getEmsToErRequestListEms(
     @TypedQuery() query: ReqEmsToErRequest.GetEmsToErRequestListQuery,
     @CurrentUser() user: EmsAuth.AccessTokenSignPayload,
-  ) {
+  ): Promise<Try<ReqEmsToErResponse.getEmsToErRequestList>> {
     const result = await this.reqEmsToErService.getEmsToErRequestList({ query, user, type: 'ems' });
     if (isError(result)) {
       return throwError(result);
@@ -73,12 +108,49 @@ export class ReqEmsToErController {
     return createResponse(result);
   }
 
+  /**
+   * ems to er 요청 리스트 조회 API - ER
+   *
+   * ER에 해당하는 요청 리스트를 조회하는 API 입니다.
+   *
+   * 본 API는 ER 시스템에서만 호출 가능합니다.
+   * ER 시스템은 본 API를 호출하여 ER에 해당하는 요청 리스트를 조회합니다.
+   *
+   *  - ER이 조회시 REQUESTED 상태인 요청은 VIEWED로 변경
+   *  - 요청을 조회하면 해당 요청은 VIEWED 상태로 변경됩니다.
+   *
+   * ## query
+   *    - page?: number;
+   *        - default: 1
+   *    - limit?: number;
+   *        - default: 10
+   *    - search?: string;
+   *        - 검색어
+   *    - search_type?: 'ambulance_company_name' | 'patient_name' | 'patient_symptom_summary';
+   *        - 검색 타입
+   *    - request_status?: RequestStatus[];
+   *        - 요청 상태 필터링
+   *    - patient_gender?: Gender[];
+   *        - 환자 성별 필터링
+   *    - patient_severity?: ems_Severity[];
+   *        - 환자 중증도 필터링
+   *    - request_start_date?: string & tags.Format<'date-time'>; // 요청 시작 날짜 및 시간
+   *        - 요청 시작 날짜 및 시간
+   *
+   * @author de-novo
+   * @tag req_ems-to-er-(ER)
+   * @summary 2023-10-08 EMS to Er 요청 리스트 조회 API - ER
+   *
+   * @security access_token
+   * @returns ems to er 요청 리스트 및 요청 리스트 개수
+   */
   @TypedRoute.Get('/er')
+  @TypedException<AUTH_ERROR.FORBIDDEN>(403, 'AUTH_ERROR.FORBIDDEN')
   @UseGuards(ErJwtAccessAuthGuard)
   async getEmsToErRequestList(
     @TypedQuery() query: ReqEmsToErRequest.GetEmsToErRequestListQuery,
     @CurrentUser() user: ErAuth.AccessTokenSignPayload,
-  ) {
+  ): Promise<Try<ReqEmsToErResponse.getEmsToErRequestList>> {
     const result = await this.reqEmsToErService.getEmsToErRequestList({ query, user, type: 'er' });
     if (isError(result)) {
       return throwError(result);
@@ -93,13 +165,44 @@ export class ReqEmsToErController {
     return createResponse(result);
   }
 
+  /**
+   * ems to er 요청 수락/거절 API - ER
+   *
+   * ER에서 요청에 대한 수락/거절을 응답하는 API 입니다.
+   *
+   * 본 API는 ER 시스템에서만 호출 가능합니다.
+   * ER 시스템은 본 API를 호출하여 ER에서 요청에 대한 수락/거절을 응답합니다.
+   *
+   *  - ER이 요청에 대한 응답을 하면 해당 요청은 ACCEPTED/REJECTED 상태로 변경됩니다.
+   *  - 요청을 응답하면 해당 요청은 ACCEPTED/REJECTED 상태로 변경됩니다.
+   *  - 만약 응답이 ACCEPTED라면 해당 요청의 다른 요청은 COMPLETED 상태로 변경됩니다.
+   *
+   * ## body
+   *    - response: 'ACCEPTED' | 'REJECTED';
+   *        - 응답
+   *
+   * ## param
+   *    - patient_id: string;
+   *        - 환자 id
+   *
+   * @summary 2023-10-08 EMS to Er 요청 수락/거절 API - ER
+   * @tag req_ems-to-er-(ER)
+   *
+   * @security access_token
+   * @returns null
+   */
   @TypedRoute.Post('/:patient_id')
+  @TypedException<AUTH_ERROR.FORBIDDEN>(403, 'AUTH_ERROR.FORBIDDEN')
+  @TypedException<REQ_EMS_TO_ER_ERROR.REQUEST_ALREADY_PROCESSED>(400.1, 'REQUEST_ALREADY_PROCESSED')
+  @TypedException<REQ_EMS_TO_ER_ERROR.REQUEST_NOT_FOUND>(404.1, 'REQUEST_NOT_FOUND')
   @UseGuards(ErJwtAccessAuthGuard)
   async respondEmsToErRequest(
     @TypedParam('patient_id') patient_id: string,
     @TypedBody() respondErToEmsRequestDto: ReqEmsToErRequest.RespondEmsToErRequestDto,
     @CurrentUser() user: ErAuth.AccessTokenSignPayload,
-  ): Promise<TryCatch<null, REQ_EMS_TO_ER_ERROR.REQUEST_ALREADY_PROCESSED | REQ_EMS_TO_ER_ERROR.REQUEST_NOT_FOUND>> {
+  ): Promise<
+    TryCatch<undefined, REQ_EMS_TO_ER_ERROR.REQUEST_ALREADY_PROCESSED | REQ_EMS_TO_ER_ERROR.REQUEST_NOT_FOUND>
+  > {
     const { response } = respondErToEmsRequestDto;
     const result = await this.reqEmsToErService.respondEmsToErRequest({
       user,
@@ -109,6 +212,9 @@ export class ReqEmsToErController {
     if (isError(result)) {
       return throwError(result);
     }
-    return createResponse(null);
+    // TODO: 카프카로 전송 필요
+    // 변경된 요청 상태를 카프카로 전송하여 EMS에게 알림
+
+    return createResponse(undefined);
   }
 }
--- src/types/req.response.dto.ts
+++ src/types/req.response.dto.ts
@@ -3,4 +3,5 @@ import { ReqEmsToEr } from '@src/providers/interface/req/req.emsToEr.interface';
 export namespace ReqEmsToErResponse {
   export interface createEmsToErRequest extends ReqEmsToEr.createEmsToErRequestReturn {}
   export interface getEmsToErRequestList extends ReqEmsToEr.GetEmsToErRequestListReturn {}
+
 }
